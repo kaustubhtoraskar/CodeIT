455b6750c8e65e560d0bcf47448c6d279eb108c1
Kaustubh Toraskar <kaustubhtoraskar97@gmail.com>
2017-11-04 02:02:07 +0530

    Register Layout

--- app/src/main/java/svkt/wallet/RegisterActivity.java
+++ app/src/main/java/svkt/wallet/RegisterActivity.java
@@ -9,5 +9,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().setTitle("Register");
     }
 }
