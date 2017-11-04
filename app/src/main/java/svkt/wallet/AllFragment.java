3e9e2fcf7686799108a52e0e1ab040a04e147675
Kaustubh Toraskar <kaustubhtoraskar97@gmail.com>
2017-11-04 12:23:53 +0530

    Register Layout

---
+++ app/src/main/java/svkt/wallet/AllFragment.java
@@ -0,0 +1,24 @@
+package svkt.wallet;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class AllFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all, container, false);
+    }
+
+
+
+
+}
