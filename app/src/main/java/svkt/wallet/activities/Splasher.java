a33b3b8f74595621c1e3faf16c4b7dec9c230b44
Kaustubh Toraskar <kaustubhtoraskar97@gmail.com>
2017-11-05 16:55:44 +0530

    final

---
+++ app/src/main/java/svkt/wallet/activities/Splasher.java
@@ -0,0 +1,15 @@
+package svkt.wallet.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import svkt.wallet.R;
+
+public class Splasher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splasher);
+    }
+}
